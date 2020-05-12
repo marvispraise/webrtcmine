59eb1eaeb1d193c1542b1173a87417277d11195c
Oluwatobi Daniels <towbeedaniels@gmail.com>
2020-05-12 13:08:49 +0100

    change

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-const express = require('express')
-const app = express()
-const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 
-app.use(express.static(__dirname + "/public"))
-let clients = 0
+app.use(express.static(__dirname + "/public"));
+let clients = 0;
 
 io.on('connection', function (socket) {
     socket.on("NewClient", function () {
         if (clients < 2) {
             if (clients == 1) {
-                this.emit('CreatePeer')
+                this.emit('CreatePeer');
             }
         } else
-            this.emit('SessionActive')
+            this.emit('SessionActive');
         clients++;
     })
-    socket.on('Offer', SendOffer)
-    socket.on('Answer', SendAnswer)
-    socket.on('disconnect', Disconnect)
+    socket.on('Offer', SendOffer);
+    socket.on('Answer', SendAnswer);
+    socket.on('disconnect', Disconnect);
 })
 
 function Disconnect() {
-    if (clients > 0) 
-        clients--
+    if (clients > 0)
+        clients--;
 } 
 
 function SendOffer(offer) {
-    this.broadcast.emit("BackOffer", offer)
+    this.broadcast.emit("BackOffer", offer);
 }
 
 function SendAnswer(data) {
-    this.broadcast.emit("BackAnswer", data)
+    this.broadcast.emit("BackAnswer", data);
 }
 
-http.listen(port, () => console.log(`Active on ${port} port`))
+http.listen(port, () => console.log(`Active on ${port} port`));
