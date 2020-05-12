b90430f0aa4ba463eaab40b95e9a6a4809875c6a
Oluwatobi Daniels <towbeedaniels@gmail.com>
2020-05-12 13:38:50 +0100

    d

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src https://*; child-src 'none';">
         <title>video Chat App</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     </head>
