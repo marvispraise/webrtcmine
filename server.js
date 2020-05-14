b93ecfe938d7d8fd99ab7dacff3e13dd0b176502
Oluwatobi Daniels <towbeedaniels@gmail.com>
2020-05-14 15:18:35 +0100

    fgh

--- server.js
+++ server.js
@@ -8,20 +8,20 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
 let clients = 0;
 
-// io.on('connection', function (socket) {
-//     socket.on("NewClient", function () {
-//         if (clients < 2) {
-//             if (clients == 1) {
-//                 this.emit('CreatePeer');
-//             }
-//         } else
-//             this.emit('SessionActive');
-//         clients++;
-//     });
-//     socket.on('Offer', SendOffer);
-//     socket.on('Answer', SendAnswer);
-//     socket.on('disconnect', Disconnect);
-// });
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('CreatePeer');
+            }
+        } else
+            this.emit('SessionActive');
+        clients++;
+    });
+    // socket.on('Offer', SendOffer);
+    // socket.on('Answer', SendAnswer);
+    // socket.on('disconnect', Disconnect);
+});
 
 // function Disconnect() {
 //     if (clients > 0)
