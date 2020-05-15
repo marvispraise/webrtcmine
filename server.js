66b6b57aa92c17e0b6fb2d0fa799ba20cd8961a4
Oluwatobi Daniels <towbeedaniels@gmail.com>
2020-05-15 10:39:52 +0100

    ded

--- server.js
+++ server.js
@@ -6,34 +6,34 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.static(__dirname + "/public"));
-// let clients = 0;
+let clients = 0;
 
-// io.on('connection', function (socket) {
-//     socket.on("NewClient", function () {
-//         if (clients < 2) {
-//             if (clients == 1) {
-//                 this.emit('createOffer');
-//             }
-//         } else
-//             this.emit('SessionActive');
-//         clients++;
-//     });
-//     // socket.on('Offer', SendOffer);
-//     // socket.on('Answer', SendAnswer);
-//     // socket.on('disconnect', Disconnect);
-// });
+io.on('connection', function (socket) {
+    socket.on("NewClient", function () {
+        if (clients < 2) {
+            if (clients == 1) {
+                this.emit('createOffer');
+            }
+        } else
+            this.emit('SessionActive');
+        clients++;
+    });
+    socket.on('Offer', SendOffer);
+    socket.on('Answer', SendAnswer);
+    socket.on('disconnect', Disconnect);
+});
 
-// function Disconnect() {
-//     if (clients > 0)
-//         clients--;
-// } 
+function Disconnect() {
+    if (clients > 0)
+        clients--;
+} 
 
-// function SendOffer(offer) {
-//     this.broadcast.emit("BackOffer", offer);
-// }
+function SendOffer(offer) {
+    this.broadcast.emit("BackOffer", offer);
+}
 
-// function SendAnswer(data) {
-//     this.broadcast.emit("BackAnswer", data);
-// }
+function SendAnswer(data) {
+    this.broadcast.emit("BackAnswer", data);
+}
 
 http.listen(port, () => console.log(`Active on ${port} port`));
